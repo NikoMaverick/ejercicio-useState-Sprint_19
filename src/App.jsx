39f9f6ff352e2ea59263291efdd6034f9f352948
Niko Maverick <maverick.nk@gmail.com>
2024-10-22 23:29:30 +0200

    1ª parte echa a falta del BONUS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import './App.css'
+import { useState } from 'react'
+
+
 
 function App() {
- //Aquí tu código
-}
+ const [name, setName] = useState('Sofia'); // Inicializamos la primera variable de estado con el nombre con Sofia
+ const [newName, setNewName] = useState (''); // La segunda variable de estado inicia con (''), sera el nuevo nombre. En este primer ejercicio no hace falta.
+
+ return (
+    <div>
+        <h2>Teacher Name: {name}</h2> {/*Este es el valor de name, inicialmente Sofia*/}
+        <ul>
+            <li onClick = {() => setName('Data')}>Data</li> {/*Con onClick, al pulsar el nombre de cada profesor, cambia el nombre del h2*/}
+            <li onClick = {() => setName('Reyes')}>Reyes</li>
+            <li onClick = {() => setName('Yolanda')}>Yolanda</li>
+        </ul>
+           
+    </div>
+ )
+};
+
 
 export default App
