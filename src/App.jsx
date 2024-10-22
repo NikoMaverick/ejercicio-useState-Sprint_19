d288e452d6bb6de0634433d13d2db7fe5b2d3e74
Niko Maverick <maverick.nk@gmail.com>
2024-10-23 00:16:48 +0200

    Bonus sin terminar, pendiente de corregir

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,52 @@ import './App.css'
 import { useState } from 'react'
 
 
+/* BONUS */
 
 function App() {
- const [name, setName] = useState('Sofia'); // Inicializamos la primera variable de estado con el nombre con Sofia
- const [newName, setNewName] = useState (''); // La segunda variable de estado inicia con (''), sera el nuevo nombre. En este primer ejercicio no hace falta.
+    const [name, setName] = useState('Sofia'); // Inicializamos la primera variable de estado con el nombre con Sofia
+    const [newName, setNewName] = useState (''); // La segunda variable de estado inicia con (''), sera el nuevo nombre. En este primer ejercicio no hace falta.
+    
+    const changeName = (input) => {
+        if(input !=='') { // Indicamos que el input no es igual a cadena vacia ''.
+            setName(input) //Actualiza el estado name con newName.
+            setNewName('') // Restablece el estado de newName a una cadena vacia.
+        }
+        
+    }
+    return (
+            <div>
+                <h2>Teacher name: {neme}</h2>
+                <form>
+                    <input type="text" value={newName} onChange={input} placeholder="add a name" />
+                    <button type="submit">Add</button>
+                </form>
+            </div>
+    )
 
- return (
-    <div>
-        <h2>Teacher Name: {name}</h2> {/*Este es el valor de name, inicialmente Sofia*/}
-        <ul>
-            <li onClick = {() => setName('Data')}>Data</li> {/*Con onClick, al pulsar el nombre de cada profesor, cambia el nombre del h2*/}
-            <li onClick = {() => setName('Reyes')}>Reyes</li>
-            <li onClick = {() => setName('Yolanda')}>Yolanda</li>
-        </ul>
+   };
+
+
+
+
+// PRIMERA PARTE
+
+//function App() {
+// const [name, setName] = useState('Sofia'); // Inicializamos la primera variable de estado con el nombre con Sofia
+// const [newName, setNewName] = useState (''); // La segunda variable de estado inicia con (''), sera el nuevo nombre. En este primer ejercicio no hace falta.
+
+// return (
+//    <div>
+//        <h2>Teacher Name: {name}</h2> {/*Este es el valor de name, inicialmente Sofia*/}
+//        <ul>
+//            <li onClick = {() => setName('Data')}>Data</li> {/*Con onClick, al pulsar el nombre de cada profesor, cambia el nombre del h2*/}
+//            <li onClick = {() => setName('Reyes')}>Reyes</li>
+//            <li onClick = {() => setName('Yolanda')}>Yolanda</li>
+//        </ul>
            
-    </div>
- )
-};
+//    </div>
+// )
+//};
 
 
 export default App
